50d392720cc7102955e9fdd88611e52bf29469b5
minde8888 <mindebaltru@gmail.com>
2022-03-22 15:57:26 +0100

    update rent

--- WTP.Data/Interfaces/IRentRepository.cs
+++ WTP.Data/Interfaces/IRentRepository.cs
@@ -11,6 +11,7 @@ namespace WTP.Data.Interfaces
         public Task AddRentToolAsync(RentDto rent);
         public Task<List<Rent>> GetRentIdAsync(Guid rentId);
         public Task<List<RentDto>> GetAllAsync(string ImageSrc);
+        public Task<Rent> UpdateRent(RentDto rentDto);
         public Task RemoveRetedToolAsync(string Id);
     }
 }
