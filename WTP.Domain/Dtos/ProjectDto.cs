b3283e9b35213f75e7e726628068990093c5e27f
minde8888 <mindebaltru@gmail.com>
2022-04-18 22:03:41 +0200

    update project

--- WTP.Domain/Dtos/ProjectDto.cs
+++ WTP.Domain/Dtos/ProjectDto.cs
@@ -10,6 +10,8 @@ namespace WTP.Domain.Dtos
         public string Title { get; set; }
         public string Place { get; set; }
         public string Status { get; set; }
+        public string Date { get; set; }    
+        public string Color { get; set; }
         public Guid? ManagerId { get; set; }
         public ICollection<ProgressPlanDto> ProgressPlan { get; set; }
     }
